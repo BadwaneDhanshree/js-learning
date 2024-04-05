d43f7de5572a8b3616f8a49bd1ed74464aaa4559
BadwaneDhanshree <163140836+BadwaneDhanshree@users.noreply.github.com>
2024-04-05 17:49:25 +0530

    some additional points

--- 06_countprimenumbers.js
+++ 06_countprimenumbers.js
@@ -6,9 +6,9 @@ function isPrime(num){
 
         return false;
     }
-     for (let i = 2; i <= Math.sqrt(num); i++){
+     for (let i = 2; i < num ; i++){
           
-        if (num % i === 0) {
+        if (num % i == 0) {
              return false;
         }
      }
@@ -25,5 +25,35 @@ function countPrimes(array){
     }
     return count++;
 }
-  console.log("Count of prime numbers:",countPrimes(array));
-    
+  console.log("Count of prime numbers:",countPrimes(array));  
+
+
+       
+  const result = isPrime(3);
+
+  console.log(`Is 3 Prime number : ${result}`);
+
+  const result1 = isPrime(9);
+
+  console.log(`Is 9 Prime number : ${result1}`);
+
+  const result2 = isPrime(7);
+
+  console.log(`Is 7 Prime number : ${result2}`);
+
+  const result3 = isPrime(6);
+
+  console.log(`Is 6 Prime number : ${result3}`);
+
+  const result4 = isPrime(19);
+
+  console.log(`Is 19 Prime number : ${result4}`);
+
+  const result5 = isPrime(29);
+
+  console.log(`Is 29 Prime number : ${result5}`);
+
+  const result6 = isPrime(53);
+
+  console.log(`Is 53 Prime number : ${result6}`);
+
